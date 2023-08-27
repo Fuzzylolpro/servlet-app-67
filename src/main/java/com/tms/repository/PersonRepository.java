6fab722219151ca3131d98d37c7de99d97e179d0
Алексей <fuzzylolpro@gmail.com>
2023-08-27 13:03:42 +0300

    JDBC created

--- src/main/java/com/tms/repository/PersonRepository.java
+++ src/main/java/com/tms/repository/PersonRepository.java
@@ -18,41 +18,42 @@ public class PersonRepository {
         try {
             Class.forName("org.postgresql.Driver");
             connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/group-67-database", "postgres", "root");
-
+            connection.setAutoCommit(false);
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-   /* public List<Person> getAll() {
-        List<Person> list = new ArrayList<>();
+    /* public List<Person> getAll() {
+         List<Person> list = new ArrayList<>();
+         try {
+             PreparedStatement statement = connection.prepareStatement("select * from person");
+             ResultSet resultSet = statement.executeQuery();
+             while (resultSet.next()) {
+                 Person person = sqlParser(resultSet);
+                 list.add(person);
+             }
+         } catch (SQLException e) {
+             System.out.println(e);
+         }
+         return list;
+     }*/
+    public Boolean create(Person person) {
         try {
-            PreparedStatement statement = connection.prepareStatement("select * from person");
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                Person person = sqlParser(resultSet);
-                list.add(person);
-            }
+            PreparedStatement statement = connection.prepareStatement("insert into person(id,first_name,second_name,age,is_married,created)" + "VALUES (default,?,?,?,?,?)");
+            statement.setString(1, person.getFirstName());
+            statement.setString(2, person.getSecondName());
+            statement.setInt(3, person.getAge());
+            statement.setBoolean(4, person.getIsMarried());
+            statement.setTimestamp(5, new Timestamp(System.currentTimeMillis()));
+            statement.executeUpdate();
+            connection.commit();
+            return true;
         } catch (SQLException e) {
             System.out.println(e);
         }
-        return list;
-    }*/
-   public Boolean create(Person person) {
-       try {
-           PreparedStatement statement = connection.prepareStatement("insert into person(id,first_name,second_name,age,is_married,created)"+"VALUES (default,?,?,?,?,?)");
-           statement.setString(1, person.getFirstName());
-           statement.setString(2, person.getSecondName());
-           statement.setInt(3, person.getAge());
-           statement.setBoolean(4, person.getIsMarried());
-           statement.setTimestamp(5,new Timestamp(System.currentTimeMillis()));
-
-           return statement.executeUpdate()>0;
-       } catch (SQLException e) {
-           System.out.println(e);
-       }
-       return false;
-   }
+        return false;
+    }
 
     public Person sqlParser(ResultSet result) throws SQLException {
         Person person = new Person();
