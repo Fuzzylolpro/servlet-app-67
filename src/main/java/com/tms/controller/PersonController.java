744f4e7d4077a46a8da65968e61d408b95eb1b3a
Алексей <fuzzylolpro@gmail.com>
2023-09-13 21:20:58 +0300

    Validation

--- src/main/java/com/tms/controller/PersonController.java
+++ src/main/java/com/tms/controller/PersonController.java
@@ -2,9 +2,13 @@ package com.tms.controller;
 
 import com.tms.domain.Person;
 import com.tms.service.PersonService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -23,23 +27,34 @@ public class PersonController {
 
     //@RequestMapping(value = "/getAll", method = RequestMethod.GET)
     @GetMapping("/getAll")
-    public String getAll(Model model){
+    public String getAll(Model model) {
         List<Person> resultList = personService.getAll();
-        if (!resultList.isEmpty()){
-            model.addAttribute("result",resultList);
+        if (!resultList.isEmpty()) {
+            model.addAttribute("result", resultList);
             return "jspPage";
         }
         return "emptyJsp";
     }
 
     @GetMapping()
-    public String getPersonId(Model model,@RequestParam("id") Long id){
-       Person resultPerson = personService.getPersonId(id);
-        if (resultPerson.getId()!=null){
-            model.addAttribute("result",resultPerson);
+    public String getPersonId(Model model, @RequestParam("id") Long id) {
+        Person resultPerson = personService.getPersonId(id);
+        if (resultPerson.getId() != null) {
+            model.addAttribute("result", resultPerson);
             return "jspPage";
         }
         return "emptyJsp";
+    }
+
+
+    @GetMapping("/is-valid")
+    public String checkValidationPerson(@Valid @ModelAttribute Person person, BindingResult bindingResult, Model model) {
+        List<ObjectError> errors = bindingResult.getAllErrors();
+        if (!bindingResult.hasErrors()){
+            return "emptyJsp";
+        }
+        model.addAttribute("result", errors);
+        return "jspPage";
     }
 
 
--- src/main/java/com/tms/domain/Person.java
+++ src/main/java/com/tms/domain/Person.java
@@ -1,11 +1,17 @@
 package com.tms.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import jakarta.validation.constraints.AssertFalse;
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.sql.Timestamp;
@@ -16,8 +22,11 @@ import java.sql.Timestamp;
 @ToString
 public class Person {
     private Long id;
+    @Size(min = 2,max = 20)
+    @NotNull
     private String firstName;
     private String secondName;
+    @Min(value = 18) @Max( value = 120)
     private Integer age;
     private Boolean isMarried;
     private Timestamp created;
